d5d180e72fdcb572442981e045341cf1c7cfb6f1
s.manandhar@concur.com <s.manandhar@sap.com>
2023-03-06 18:20:10 -0800

    disabled button

--- code/my-firstapp1/src/hooks/IncrementDecrement.js
+++ code/my-firstapp1/src/hooks/IncrementDecrement.js
@@ -11,24 +11,33 @@ const IncrementDecrement = () => {
     <>
       <h2 className="text-center">{number}</h2>
       <center>
-        <button
-          // onClick={increase}
-          className="btn btn-primary"
-          onClick={() => {
-            setNumber(number + 1);
-          }}
-        >
-          Increment
-        </button>
-        <button
-          // onClick={increase}
-          className="btn btn-primary"
-          onClick={() => {
-            setNumber(number - 1);
-          }}
-        >
-          Decrement
-        </button>
+        {number < 10 && (
+          <button
+            // onClick={increase}
+            className="btn btn-primary"
+            onClick={() => {
+              setNumber(number + 1);
+            }}
+          >
+            Increment
+          </button>
+        )}
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        {number > 0 && (
+          <button
+            id="decrement"
+            // onClick={increase}
+            // disabled={!number==1}
+            className="btn btn-primary"
+            onClick={() => {
+              if (number > 1) {
+                setNumber(number - 1);
+              }
+            }}
+          >
+            Decrement
+          </button>
+        )}
       </center>
     </>
   );
