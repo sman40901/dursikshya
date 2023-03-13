9cc721d16f0f64abda997d13a9cc5c195bd04563
s.manandhar@concur.com <s.manandhar@sap.com>
2023-03-12 18:40:07 -0700

    11 march 2023

--- code/my-firstapp1/src/components/Card.js
+++ code/my-firstapp1/src/components/Card.js
@@ -1,50 +1,82 @@
-import React from 'react'
+import React from "react";
+import './card.css'
 
-const Card = () => {
+const Card = (props) => {
+  const { productName, productPrice, productImage } = props;
+  // above statement is equivalent to
+  // const productName=props.productName;
   return (
     <>
-    <div className="container my-3">
-    <div className="row row-cols-1 row-cols-md-4 g-4">
-  <div className="col">
-    <div className="card shadow p-1">
-      <img src="https://images.unsplash.com/photo-1677847240822-a420f69cb98f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw2fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60" className="card-img-top" alt="..."/>
-      <div className="card-body">
-        <h5 className="card-title">Card title</h5>
-        <a href="" className='btn btn-primary'>View Details</a>
+      {/* we are moving these 2 div tags to upper component 
+     so that iteration thru items would be easier */}
+      {/* <div className="container my-3">
+        <div className="row row-cols-1 row-cols-md-4 g-4"> */}
+      <div className="col">
+        <div className="card shadow p-1">
+          <img
+            src={productImage}
+            className="card-img-top"
+            alt="..."
+          />
+          <div className="card-body">
+            <h5 className="card-title">{productName.slice(0,20)}</h5>
+            <h5>${productPrice}</h5>
+            <a href="" className="btn btn-primary">
+              View Details
+            </a>
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
-  <div className="col">
-    <div className="card">
-      <img src="https://plus.unsplash.com/premium_photo-1661913022485-f5931b1f4d56?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxM3x8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=60" className="card-img-top" alt="..."/>
-      <div className="card-body">
-        <h5 className="card-title">Card title</h5>
-        <a href="" className='btn btn-primary'>View Details</a>
-      </div>
-    </div>
-  </div>
-  <div className="col">
-    <div className="card">
-      <img src="https://unsplash.com/photos/PjVB8R9Jm-8" className="card-img-top" alt="..."/>
-      <div className="card-body">
-        <h5 className="card-title">Card title</h5>
-        <a href="" className='btn btn-primary'>View Details</a>
-      </div>
-    </div>
-  </div>
-  <div className="col">
-    <div className="card">
-      <img src="https://unsplash.com/photos/Z_LQcnGO7sM" className="card-img-top" alt="..."/>
-      <div className="card-body">
-        <h5 className="card-title">Card title</h5>
-        <a href="" className='btn btn-primary'>View Details</a>
-      </div>
-    </div>
-  </div>
-</div>
-    </div>
+      {/* not used any more all the col code below this line */}
+      {/* <div className="col">
+            <div className="card">
+              <img
+                src="https://plus.unsplash.com/premium_photo-1661913022485-f5931b1f4d56?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxM3x8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=60"
+                className="card-img-top"
+                alt="..."
+              />
+              <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <a href="" className="btn btn-primary">
+                  View Details
+                </a>
+              </div>
+            </div>
+          </div> */}
+      {/* <div className="col">
+            <div className="card">
+              <img
+                src="https://unsplash.com/photos/PjVB8R9Jm-8"
+                className="card-img-top"
+                alt="..."
+              />
+              <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <a href="" className="btn btn-primary">
+                  View Details
+                </a>
+              </div>
+            </div>
+          </div> */}
+      {/* <div className="col">
+            <div className="card">
+              <img
+                src="https://unsplash.com/photos/Z_LQcnGO7sM"
+                className="card-img-top"
+                alt="..."
+              />
+              <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <a href="" className="btn btn-primary">
+                  View Details
+                </a>
+              </div>
+            </div>
+          </div> */}
+      {/* </div>
+      </div> */}
     </>
-  )
-}
+  );
+};
 
-export default Card
+export default Card;
--- code/my-firstapp1/src/pages/Home.js
+++ code/my-firstapp1/src/pages/Home.js
@@ -1,21 +1,43 @@
-import React from 'react'
-import Layouts from '../components/Layouts'
-import Slider from '../components/Slider'
-import Card from '../components/Card'
-import IncrementDecrement from '../hooks/IncrementDecrement'
-import Effect from '../hooks/Effect'
-import DataFetch from '../hooks/DataFetch'
+import React, { useState, useEffect } from "react";
+// we are importing useState and useEffect to transfer data from parent to child componet
+import Layouts from "../components/Layouts";
+import Slider from "../components/Slider";
+import Card from "../components/Card";
+import IncrementDecrement from "../hooks/IncrementDecrement";
+import Effect from "../hooks/Effect";
+import DataFetch from "../hooks/DataFetch";
+import axios from "axios";
 
 const Home = () => {
+  const stdLimit = 8;
+  const [products, setProducts] = useState([]);
+  const [limit, setLimit] = useState(stdLimit);
+
+  useEffect(() => {
+    axios
+      .get("https://fakestoreapi.com/products")
+      .then((res) => {
+        // console.log(res)
+        setProducts(res.data);
+      })
+      .catch((err) => console.log(err));
+  }, []); // show effect on page load
+  //empty arry is passed to show effect only once
   return (
     <>
-    <Slider/>
-    <Card/>
-    <IncrementDecrement/>
-    <Effect/>
-    <DataFetch/>
+      <Slider />
+      <div className="container my-3">
+        <div className="row row-cols-1 row-cols-md-4 g-4">
+          {products && products.map((product,index)=>{
+          return <Card key={index} productName={product.title} productPrice={product.price} productImage={product.image}/>
+        })}
+        </div>
+      </div>
+      <IncrementDecrement />
+      <Effect />
+      <DataFetch />
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
