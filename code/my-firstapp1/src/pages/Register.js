8da53c8d96004768746b8ab1016754eb1f963690
sman40901 <sujanmanpr@gmail.com>
2023-03-16 19:16:36 -0700

    form validation

--- code/my-firstapp1/src/pages/Register.js
+++ code/my-firstapp1/src/pages/Register.js
@@ -1,12 +1,39 @@
 import React from "react";
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup'; // all function import
 
 const Register = () => {
   return (
-    <>
+    <Formik
+      initialValues={{ firstname: '', lastname: '', email: '', password: '', cpassword: '' }}
+      validationSchema={Yup.object({
+        firstname: Yup.string()
+          .max(20, "must be 20 chars of less")
+          .required('Firstname is mandatory'),
+
+        lastname: Yup.string()
+          .max(20, "must be 20 chars of less")
+          .required('Lastname is mandatory'),
+
+        email: Yup.string()
+          .email("Invalid email address")
+          .required('Email is mandatory'),
+
+        password: Yup.string()
+          // .matches(/(?=.*[A-Za-z])(?=.*\d)(?=.*[@#$%^&*!_-+=])[A-Za-z\d@#$%^&*!_-+=]{8,}$/,"password is not valid")
+          .required('password is mandatory'),
+
+        cpassword: Yup.string()
+          // .matches(/(?=.*[A-Za-z])(?=.*\d)(?=.*[@#$%^&*!_-+=])[A-Za-z\d@#$%^&*!_-+=]{8,}$/,"password is not valid")
+          .required('password is mandatory')
+          .oneOf([Yup.ref('password'), null], "password does not match")
+
+      })}
+    >
       <div className="container">
         <div className="row d-flex justify-content-center">
           <dic className="col-md-4 shadow p-3">
-            <form className="row g-3">
+            {/* <form className="row g-3">
               <div className="col-md-6">
                 <label for="inputEmail4" className="form-label">
                   Email
@@ -83,11 +110,51 @@ const Register = () => {
                   Sign in
                 </button>
               </div>
-            </form>
+            </form> */}
+            <Form>
+              <div className="mb-3">
+                <label htmlFor="firstname">FirstName</label>
+                <Field type='text' id='firstname' name='firstname' className='form-control'></Field>
+                <ErrorMessage name='firstname'>
+                  {msg => <div style={{ color: 'red' }}>{msg}</div>}
+                </ErrorMessage>
+              </div>
+              <div className="mb-3">
+                <label htmlFor="lastname">LastName</label>
+                <Field type='text' id='lastname' name='lastname' className='form-control'></Field>
+                <ErrorMessage name='lastname'>
+                  {msg => <div style={{ color: 'red' }}>{msg}</div>}
+                </ErrorMessage>
+              </div>
+              <div className="mb-3">
+                <label htmlFor="email">Email</label>
+                <Field type='email' id='email' name='email' className='form-control'></Field>
+                <ErrorMessage name='email'>
+                  {msg => <div style={{ color: 'red' }}>{msg}</div>}
+                </ErrorMessage>
+              </div>
+              <div className="mb-3">
+                <label htmlFor="password">Password</label>
+                <Field type='password' id='password' name='password' className='form-control'></Field>
+                <ErrorMessage name='password'>
+                  {msg => <div style={{ color: 'red' }}>{msg}</div>}
+                </ErrorMessage>
+              </div>
+              <div className="mb-3">
+                <label htmlFor="cpassword">Confirm password</label>
+                <Field type='password' id='cpassword' name='cpassword' className='form-control'></Field>
+                <ErrorMessage name='cpassword'>
+                  {msg => <div style={{ color: 'red' }}>{msg}</div>}
+                </ErrorMessage>
+              </div>
+              <div className="mb-2">
+                <button className="btn btn-primary">Register</button>
+              </div>
+            </Form>
           </dic>
         </div>
       </div>
-    </>
+    </Formik>
   );
 };
 
