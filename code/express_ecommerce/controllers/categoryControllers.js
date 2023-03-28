29250def601b88a3cfbf475c1da8bb4e3b25eda5
sman40901 <sujanmanpr@gmail.com>
2023-03-27 19:23:18 -0700

    put, post, delete, get added

--- code/express_ecommerce/controllers/categoryControllers.js
+++ code/express_ecommerce/controllers/categoryControllers.js
@@ -10,9 +10,91 @@ exports.postCategory = async (req, res) => {
     let category = new Category({
         category_name: req.body.category_name
     });
-    category = await category.save();
+    // to check if data already exists
+    // Category.findOne({ category_name: category.category_name }, async (error, data) => {
+    //     if (/*!error || */
+    //         data == null) {
+    //         category = await category.save();
+    //         if (!category) {
+    //             return res.status(400).json({ error: 'provided category format is wrong' });
+    //         }
+    //         res.send(category);
+    //     }
+    //     else {
+    //         return res.status(400).json({ error: 'category must be unique, this already exists' })
+    //     }
+    // });
+    Category.findOne({ category_name: category.category_name }) // finds element from database not in memory
+        .then(async categories => {
+            if (categories) {
+                return res.status(400).json({ error: 'category must be unique, this already exists' })
+            }
+            else {
+                category = await category.save();
+                if (!category) {
+                    return res.status(400).json({ error: 'provided category format is wrong' });
+                }
+                res.send(category);
+            }
+        })
+        .catch(err => res.status(400).json({ error }))
+    // category = await category.save();
+    // if (!category) {
+    //     return res.status(400).json({ error: 'provided category format is wrong' });
+    // }
+    // res.send(category);
+};
+
+//to show all category
+exports.categoryList = async (req, res) => {
+    const category = await Category.find();
     if (!category) {
-        return res.status(400).json({ error: 'provided category format is wrong' });
+        return res.status(400).json({ error: 'something went wrong' });
     }
     res.send(category);
-};
+}
+
+exports.categoryDetails = async (req, res) => {
+    const category = await Category.findById(req.params.id);
+    if (!category) {
+        return res.status(400).json({ error: 'id not found' });
+    }
+    res.send(category);
+}
+
+exports.categoryUpdate = async (req, res) => {
+    const category = await Category.findByIdAndUpdate(
+        req.params.id,
+        {
+            category_name: req.body.category_name
+        },
+        {
+            // this is to show updated value 
+            new:true
+        }
+    );
+    if (!category) {
+        return res.status(400).json({ error: 'id not found' });
+    }
+    res.send(category);
+}
+
+exports.categoryDelete =  (req, res) => {
+    const category = Category.findByIdAndRemove(
+        req.params.id
+    )
+    .then(category=>{
+        if (!category) {
+            return res.status(403).json({ error: 'id not found' });
+        }
+        else
+        {
+            return res.status(200).json({message:'category deleted'})
+        }
+    })
+    .catch(err=>{
+        return res.status(400).json({error:'id not found'})
+    })
+   
+    // res.send(category);
+}
--- code/express_ecommerce/routes/categoryRoutes.js
+++ code/express_ecommerce/routes/categoryRoutes.js
@@ -1,8 +1,14 @@
 const express=require('express');
-const { testFunction, postCategory } = require('../controllers/categoryControllers');
+const { testFunction, postCategory, categoryList, categoryDetails, categoryUpdate, categoryDelete } = require('../controllers/categoryControllers');
+
 const router=express.Router();
 
 
 router.get('/demo',testFunction); //since function is getting called we dont need paranthesis
 router.post('/postcategory',postCategory);
+router.get('/categorylist',categoryList);
+router.get('/categorydetails/:id',categoryDetails);
+router.put('/categoryupdate/:id',categoryUpdate);
+router.delete('/categorydelete/:id',categoryDelete);
+
 module.exports=router
