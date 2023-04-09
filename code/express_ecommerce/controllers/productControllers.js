b36986d03747cb8a59ea07354b01f0cde09e79d1
sman40901 <sujanmanpr@gmail.com>
2023-04-09 10:35:51 -0700

    product sort not working

--- code/express_ecommerce/controllers/productControllers.js
+++ code/express_ecommerce/controllers/productControllers.js
@@ -22,8 +22,8 @@ exports.productPost = async (req, res) => {
 //to show all products
 exports.productList = async (req, res) => {
     const product = await Product.find()
-        .populate('category', 'category_name')
-        // .sort(createdAt); // inner join using category forieng key
+        .populate('category', 'category_name') // inner join using category forieng key
+        // .sort(createdAt); 
     // if (req.params.order) {
     //     if (req.params.order == 'asc') {
     //         console.log("product ascending");
