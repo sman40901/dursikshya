62f3a3e2c0dd710cc41aa9b5c9fc245fa249b608
sman40901 <sujanmanpr@gmail.com>
2023-08-14 18:49:41 -0700

    product error fixed

--- code/express-api/controllers/productController.js
+++ code/express-api/controllers/productController.js
@@ -2,7 +2,7 @@ const Product = require('../models/productModel');
 
 // to post product
 exports.postProduct = async (req, res) => {
-    let products = new Product({
+    let product = new Product({
         product_name: req.body.product_name, // you can use any variable name but for easiness we use the same name
         product_price:req.body.product_price,
         countInStock:req.body.countInStock,
@@ -10,19 +10,28 @@ exports.postProduct = async (req, res) => {
         product_image:req.body.product_image,
         category:req.body.Category
     });
-    // check for unique data
-    Product.findOne({ product_name: product.product_name })
-        .then(async prod => {
-            if (prod) {
-                return res.status(400).json({ error: 'product name already exists in database' });
-            } else {
-                product = await product.save();
 
-                if (!product) {
-                    return res.status(400).json({ error: 'something went wrong, check your data format' });
-                }
-                res.send(product);
-            }
-        })
-        .catch(err => res.status(500).json({ error: err }))
+    product=await product.save()
+    if(!product){
+        return res.status(400).json({error:"error"})
+    }
+    res.send (product)
+}
+
+// to show the list of categories
+exports.getProductList = async (req, res) => {
+    const product = await Product.find();
+    if (!product) {
+        return res.status(400).json({ error: 'something went wrong' });
+    }
+    res.send(product);
+}
+
+// to show a category and its details
+exports.getCategoryById = async (req, res) => {
+    const category = await Category.findById(req.params.id);
+    if (!category) {
+        return res.status(400).json({ error: 'something went wrong' });
+    }
+    res.send(category);
 }
--- code/express-api/routes/productRoute.js
+++ code/express-api/routes/productRoute.js
@@ -3,3 +3,5 @@ const { postProduct } = require('../controllers/productController');
 const router = express.Router();
 
 router.post('/postproduct', postProduct);
+
+module.exports = router // exporting using default method
